49d79e86caebd88296d4efbe8e0fa52a874c7ed6
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2025-03-22 21:47:23 +0100

    authentication + crud + admin user actions

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -63,9 +63,9 @@ def authenticate_user(username: str, password: str, db):
     return user
 
 
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
-    encode = {'sub': username, 'id': user_id, 'exp': expires}
+    encode = {'sub': username, 'id': user_id, 'exp': expires, 'role': role}
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
@@ -74,9 +74,10 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
+        user_role: str = payload.get('role')
         if username is None or user_id is None:
             raise_401_could_not_validate_user()
-        return {'username': username, 'id': user_id}
+        return {'username': username, 'id': user_id, 'user_role': user_role}
     except JWTError:
         raise_401_could_not_validate_user()
 
@@ -104,5 +105,5 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise_401_could_not_validate_user()
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
     return {'access_token': token, 'token_type': 'bearer'}
