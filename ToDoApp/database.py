4f08133914e18a907fe82273ea5c45bf40c34389
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2025-03-24 23:01:33 +0100

    working with postgresql and mysql

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -1,10 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from db_secret import postgresql_password as db_password
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = f'postgresql://postgres:{db_password}@localhost/TodoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://root:{db_password}@127.0.0.1:3306/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
